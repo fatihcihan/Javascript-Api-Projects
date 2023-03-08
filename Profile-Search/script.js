6595c3810d31f73a395eb3be24571b18845fd54d
Fatih <fatihcihan666@gmail.com>
2023-03-09 02:54:40 +0300

    Translation done

--- Profile-Search/script.js
+++ Profile-Search/script.js
@@ -1,5 +1,9 @@
 const fromLanguage = document.querySelector("#from-lang");
 const toLanguage = document.querySelector("#to-lang");
+const btnTranslate = document.querySelector("#btnTranslate");
+const fromText = document.querySelector("#from-text");
+const toText = document.querySelector("#to-text");
+
 
 for (let language in languages) {
     let option = `<option value="${language}">${languages[language]}</option> `;
@@ -9,3 +13,17 @@ for (let language in languages) {
     fromLanguage.value = "tr-TR";
     toLanguage.value = "en-GB";
 }
+
+btnTranslate.addEventListener("click", () => {
+    let text = fromText.value;
+    let fromLang = fromLanguage.value;
+    let toLang = toLanguage.value;
+    const url = `https://api.mymemory.translated.net/get?q=${text}!&langpair=${fromLang}|${toLang}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            toText.value = data.responseData.translatedText;
+        });
+})
+
+
