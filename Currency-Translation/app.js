66252cfdf62c1eb698a14194b4eacb05ef3532f4
Fatih <fatihcihan666@gmail.com>
2023-03-08 11:50:42 +0300

    currency symbols introduced

---
+++ Currency-Translation/app.js
@@ -0,0 +1,25 @@
+const apiKey = "c7928bbe9ac3ecfe36e30793";
+const url = `https://v6.exchangerate-api.com/v6/${apiKey}`;
+
+// elements
+
+const currency_one = document.getElementById('currency_one');
+const currency_two = document.getElementById('currency_two');
+const list_one = document.getElementById("list_one");
+const list_two = document.getElementById("list_two");
+const amount = document.getElementById("amount");
+const calculate = document.getElementById("calculate");
+const result = document.getElementById("result");
+
+fetch(url + "/codes")
+    .then(res => res.json())
+    .then(data => {
+        const items = data.supported_codes;
+        let options;
+        for (let item of items) {
+            console.log(item);
+            options += `<option value=${item[0]}>${item[1]}</option>`
+        }
+        list_one.innerHTML = options;
+        list_two.innerHTML = options;
+    })
