29433612823d5dfb674b58c3d58509eda73f980b
Fatih <fatihcihan666@gmail.com>
2023-03-11 14:25:02 +0300

    Web speech api used

--- Profile-Search/script.js
+++ Profile-Search/script.js
@@ -4,7 +4,7 @@ const btnTranslate = document.querySelector("#btnTranslate");
 const fromText = document.querySelector("#from-text");
 const toText = document.querySelector("#to-text");
 const exchange = document.querySelector(".exchange");
-
+const icons = document.querySelectorAll(".icons")
 
 for (let language in languages) {
     let option = `<option value="${language}">${languages[language]}</option> `;
@@ -37,3 +37,25 @@ exchange.addEventListener("click", () => {
     fromLanguage.value = toLanguage.value;
     toLanguage.value = language;
 });
+
+for (let icon of icons) {
+    icon.addEventListener("click", (element) => {
+        if (element.target.classList.contains("fa-copy")) {
+            if (element.target.id == "from") {
+                navigator.clipboard.writeText(fromText.value);
+            } else {
+                navigator.clipboard.writeText(toText.value);
+            }
+        } else {
+            let utterance;
+            if (element.target.id == "from") {
+                utterance = new SpeechSynthesisUtterance(fromText.value);
+                utterance.lang = fromLanguage.value;
+            } else {
+                utterance = new SpeechSynthesisUtterance(toText.value);
+                utterance.lang = toLanguage.value;
+            }
+            speechSynthesis.speak(utterance);
+        };
+    });
+}
