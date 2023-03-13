9dded2ba35f997d0e13a55f34f41a3c3b6b70b40
Fatih <fatihcihan666@gmail.com>
2023-03-14 01:05:14 +0300

    Added show todo method

--- Translator/ui.js
+++ Translator/ui.js
@@ -37,6 +37,10 @@ class UI {
                                 company : ${profile.company.name}
                            </li>
                         </ul>
+                        <h4 class="mt-4">Todo List</h4>
+                        <ul id="todo" class="list-group">
+                        
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -50,6 +54,29 @@ class UI {
 
     showAlert(text) {
         this.alert.innerHTML = `${text} is not found. `;
+    }
+
+    showTodo(todo) {
+        let html = "";
+
+        todo.forEach(item => {
+
+            if (item.completed) {
+                html += `
+                        <li class="list-group-item bg-success">
+                            ${item.title}
+                        </li>
+                `;
+            } else {
+                html += `
+                        <li class="list-group-item bg-secondary">
+                            ${item.title}
+                        </li>
+                `;
+            }
+        });
+
+        this.profileContainer.querySelector("#todo").innerHTML = html;
     }
 
     clear() {
