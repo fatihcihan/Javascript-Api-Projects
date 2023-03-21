91ebe09af80514f9f82d6f3d60ef663538650e79
Fatih <fatihcihan666@gmail.com>
2023-03-21 11:33:53 +0300

    data structure created

--- Oem-Calculator/app.js
+++ Oem-Calculator/app.js
@@ -10,10 +10,30 @@ const StorageController = (function () {
 const ProductController = (function () {
 
     // private 
+    const Product = function (id, name, price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    const data = {
+        products: [
+            { id: 0, name: 'Monitor', price: 200 },
+            { id: 1, name: 'Ram', price: 500 },
+            { id: 2, name: 'Mouse', price: 100 }
+        ],
+        selectedProduct: null,
+        totalPrice: 0
+    }
 
     // public
     return {
-
+        getProdcuts: function () {
+            return data.products;
+        },
+        getData: function () {
+            return data;
+        }
     }
 
 })();
@@ -32,6 +52,15 @@ const UIController = (function () {
 
 
 // App Controller
-const AppController = (function (ProductCtrl, UICtrl) {
+const App = (function (ProductCtrl, UICtrl) {
+    return {
+        init: function () {
+            console.log('starting app');
+            const products = ProductCtrl.getProdcuts();
 
+            UIController.createProductList(products);
+        }
+    }
 })(ProductController, UIController);
+
+App.init();
